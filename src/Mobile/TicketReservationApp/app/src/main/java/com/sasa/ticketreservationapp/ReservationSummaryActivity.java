e699fa72ff71d75ac3dfcadea924a1d28f43dae8
Dulshan Perera <s.dulshan.perera@gmail.com>
2023-09-27 11:22:15 +0530

    feat: Implemented ReservationSummary screen

--- src/Mobile/TicketReservationApp/app/src/main/java/com/sasa/ticketreservationapp/ReservationSummaryActivity.java
+++ src/Mobile/TicketReservationApp/app/src/main/java/com/sasa/ticketreservationapp/ReservationSummaryActivity.java
@@ -1,14 +1,14 @@
 package com.sasa.ticketreservationapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class ReservationSummaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_reservation_summary);
     }
 }
