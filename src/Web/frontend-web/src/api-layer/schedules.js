5e1c453a64ec4cc6cfc0240bf79541305a57b72e
Shehanx86 <your-email@example.com>
2023-10-10 22:39:52 +0530

    Ticket reservation dialog

--- src/Web/frontend-web/src/api-layer/schedules.js
+++ src/Web/frontend-web/src/api-layer/schedules.js
@@ -3,11 +3,12 @@ import HttpServiceConfig from '../configs/http-service-config'
 class ScheduleAPIService {
     getScheduleTrainsData = (req, headers = {}) => {
         const formattedReq = {
-            destinationStationId: req.trainName,
-            startPointStationId: req.seatCapacity,
-            dateTime: req.availableDays,
-            passengerClass: req.passengerClasses,
+            destinationStationId: req.destinationStationId,
+            startPointStationId: req.startPointStationId,
+            dateTime: req.dateTime,
+            passengerClass: req.passengerClass,
           }
+
         return new Promise((resolve, reject) => {
           HttpServiceConfig.post('/api/Schedule/getScheduleTrainsData', formattedReq, headers)
             .then((data) => {
--- src/Web/frontend-web/src/modules/bookings/BookingDialog.js
+++ src/Web/frontend-web/src/modules/bookings/BookingDialog.js
@@ -32,17 +32,25 @@ const BookingDialog = ({ settings, onClose, onSave, callBackData }) => {
   const [isSchedulesLoading, setSchedulesLoading] = useState(true);
   const [schedules, setSchedules] = useState([]);
   const [formDataselectedPClass, setFormDataSelectedPclass] = useState(1);
+  const [selectedTrain, setSelectedTrain] = useState({});
 
   const handlePClassOnChange = (e) => {
     console.log(e.target.value)
     setFormDataSelectedPclass(e.target.value)
   }
 
+  const handleSelectTime = (e) => {
+    setSelectedTrain(e.target.value)
+    console.log(e)
+
+  }
+  
   const getAllStations = async () => {
     try {
         setStationsLoading(true)
         const stationsRes = await MasterDataAPIService.getAllStationMasterData()
         const _stations = stationsRes.map(station => ({ label: station.name, id: station.id }));
+        if (Array.isArray(_stations) && _stations.length > 0) setShedulesAvailability(true)
         setStations(_stations);
     } catch(error) {
         console.log(error)
@@ -55,16 +63,16 @@ const BookingDialog = ({ settings, onClose, onSave, callBackData }) => {
 
   const checkTrainAvailability = async () => {
     try {
-        console.log(formDataDestination[0].id)
-        console.log(formDataOrigin)
-        console.log(formDataStartDate)
-        console.log(formDataselectedPClass)
-
+        const reqObj = {
+            destinationStationId: formDataDestination[0].id,
+            startPointStationId: formDataOrigin[0].id,
+            dateTime: formDataStartDate.toLocaleDateString('en-CA'),
+            passengerClass: parseInt(formDataselectedPClass),
+        }
         setSchedulesLoading(true)
-        const stationsRes = await ScheduleAPIService.getScheduleTrainsData(
-
-        )
-        setSchedules(stationsRes)
+        const _schedules = await ScheduleAPIService.getScheduleTrainsData(reqObj)
+        console.log(_schedules)
+       setSchedules(_schedules)
     } catch (error) {
         console.log(error)
         setSchedules([])
@@ -260,44 +268,74 @@ const BookingDialog = ({ settings, onClose, onSave, callBackData }) => {
                   </Row>
                 </div>
                 {isScheduleAvailable && !isSchedulesLoading && 
-                    <Row style={{ marginTop: '20px' }}>
-                    <Col sm={12}>
-                        <Form.Group style={{}}>
-                        <Form.Label>Passenger Classes*</Form.Label>
-                        {TRAIN_PASSENGER_CLASSES.filter((d) => d.id !== 0).map(
-                            (item, index) => (
-                            <div key={index}>
-                                <label>
-                                <input
-                                    type="checkbox"
-                                    name="passengerClasses"
-                                    value={item.id}
-                                    checked={values.passengerClasses.includes(
-                                    item.id,
-                                    )}
-                                    onChange={(e) => {
-                                    const isChecked = e.target.checked
-                                    const newValue = isChecked
-                                        ? [...values.passengerClasses, item.id]
-                                        : values.passengerClasses.filter(
-                                            (id) => id !== item.id,
-                                        )
-                                    setFieldValue('passengerClasses', newValue)
-                                    }}
-                                />
-                                {item.name}
-                                </label>
-                            </div>
-                            ),
-                        )}
-                        <ErrorMessage
-                            name="passengerClasses"
-                            component="div"
-                            className="text-danger"
-                        />
-                        </Form.Group>
-                    </Col>
-                    </Row>
+                <>
+            <Row style={{ marginTop: '10px' }}>
+            <Col sm={12}>
+              <Form.Group style={{}}>
+                <Form.Label>Time*</Form.Label>
+                <Field
+                  onChange={e => handleSelectTime(e, setFieldValue)}
+                  value={selectedTrain.arrivalTime}
+                  name="availableDays"
+                  as="select"
+                  style={{
+                    width: '100%',
+                    height: '37px',
+                    border: '1px solid #dee2e6',
+                    borderRadius: '0.375rem',
+                  }}
+                >
+                  {schedules.map((train, index) => (
+                    <option key={index} value={JSON.stringify(train)}>
+                      {new Date(train.arrivalTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' })}
+                    </option>
+                  ))}
+
+                </Field>
+              </Form.Group>
+            </Col>
+            </Row>
+            <Row style={{ marginTop: '10px' }}>
+            <Col sm={12}>
+              <Form.Group style={{}}>
+                <Form.Label>Train</Form.Label>
+
+                        <Form.Control value={JSON.parse(selectedTrain).trainName} />
+                      {/* </Field> */}
+                    
+                  
+        
+              </Form.Group>
+            </Col>
+            
+            </Row>
+            <Row style={{ marginTop: '10px' }}>
+            <Col sm={6}>
+                    <Form.Group>
+                      <Form.Label>Passenger count*</Form.Label>
+                      <Field name="seatCapacity">
+                        {({ field }) => <Form.Control {...field}/>}
+                      </Field>
+                      <ErrorMessage
+                        name="seatCapacity"
+                        component="div"
+                        className="text-danger"
+                      />
+                    </Form.Group>
+                </Col>
+            <Col sm={6}>
+              <Form.Group style={{}}>
+                <Form.Label>Price</Form.Label>
+   
+                    <Field name="trainName" value={selectedTrain.trainName || "cat"}>
+                        {({ field }) => <Form.Control value={selectedTrain.trainName || "cat"} {...field} />}
+                      </Field>
+                  
+        
+              </Form.Group>
+            </Col>
+            </Row>
+            </>
                 }
                 <div
                   style={{
